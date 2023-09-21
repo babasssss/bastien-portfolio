447ff09e0bae75be340dd04b6ec7bc5ecc5c0210
SOREAU BASTIEN <80333079+babasssss@users.noreply.github.com>
2023-09-21 18:16:21 +0200

    add work

--- components/Nav.js
+++ components/Nav.js
@@ -14,16 +14,7 @@ export const navData = [
   { name: 'about', path: '/about', icon: <HiUser /> },
   { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
   { name: 'work', path: '/work', icon: <HiViewColumns /> },
-  {
-    name: 'testimonials',
-    path: '/testimonials',
-    icon: <HiChatBubbleBottomCenterText />,
-  },
-  {
-    name: 'contact',
-    path: '/contact',
-    icon: <HiEnvelope />,
-  },
+  { name: 'contact', path: '/contact', icon: <HiEnvelope />, },
 ];
 
 // Next link
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,5 +1,51 @@
+// import
+import WorkSlider from '../../components/WorkSlider'
+import Bulb from '../../components/Bulb'
+import Circle from '../../components/Circles'
+import {motion} from 'framer-motion'
+import {fadeIn} from '../../variants'
+
+
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div className='h-full bg-primary/30 py-36 flex items-center'>
+      <Circle/>
+      <div className='container mx-auto'>
+        <div className='flex flex-col xl:flex-row gap-x-8'>
+
+          {/* text */}
+          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4'>
+            <motion.h2
+              variants={fadeIn('up', 0.2)} 
+              initial="hidden" 
+              animate="show" 
+              exit="hidden"   
+              className='h2 xl:mt-12'>
+                My work <span className='text-accent'>.</span>
+            </motion.h2>
+            <motion.p 
+              variants={fadeIn('up', 0.4)} 
+              initial="hidden" 
+              animate="show" 
+              exit="hidden"  
+              className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
+              Mollisqdft fugiat nulla aute qui dolor eu sit est magna. Ut esse in nisi irure cupidatat dolore fugiat nisi amet quis.
+            </motion.p>
+          </div>
+          <motion.div 
+            variants={fadeIn('down', 0.6)} 
+            initial="hidden" 
+            animate="show" 
+            exit="hidden"  
+            className='w-full xl:max-w-[65%]'>
+            {/* Slider */}
+            <WorkSlider/>
+          </motion.div>
+        </div>
+      </div>
+      <Bulb/>
+    </div>
+  );
 };
 
 export default Work;
